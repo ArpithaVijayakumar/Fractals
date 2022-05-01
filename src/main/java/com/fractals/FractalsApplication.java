48faac7bca7d8d0806a409e233f5ac8f3dc104af
Arpitha Vijayakumar <arpithav@icloud.com>
2022-05-01 15:20:05 -0400

    Initial setup to run in tomcat

--- src/main/java/com/fractals/FractalsApplication.java
+++ src/main/java/com/fractals/FractalsApplication.java
@@ -3,11 +3,17 @@ package com.fractals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAsync
-public class FractalsApplication
-{
+
+public class FractalsApplication extends SpringBootServletInitializer {
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FractalsApplication.class);
+	}
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(FractalsApplication.class, args);
